bc089c74813a883ff94a6577d1249b4bb68c084e
Gayatri Chawada <111906949+Gcc1012@users.noreply.github.com>
2025-06-06 11:57:59 +0530

    Update load_data.py

--- image_classification/load_data.py
+++ image_classification/load_data.py
@@ -7,6 +7,6 @@ from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
 def load_data(data_dir, img_size=(224, 224), batch_size=32):
     datagen = ImageDataGenerator(validation_split=0.2, rescale=1./255)
-    train_gen = datagen.flow_from_directory("data", target_size=img_size, batch_size=batch_size, class_mode='categorical', subset='training')
-    val_gen = datagen.flow_from_directory(data_dir, target_size=img_size, batch_size=batch_size, class_mode='categorical', subset='validation')
+    train_gen = datagen.flow_from_directory("data", target_size=img_size, batch_size=batch_size, class_mode='binary', subset='training')
+    val_gen = datagen.flow_from_directory(data_dir, target_size=img_size, batch_size=batch_size, class_mode='binary', subset='validation')
     return train_gen, val_gen
