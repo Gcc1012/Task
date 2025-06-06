093cb3f2a56569dd5b21d7782b3020a73c95af7c
Gayatri Chawada <111906949+Gcc1012@users.noreply.github.com>
2025-06-06 12:00:48 +0530

    Update main.py

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ warnings.filterwarnings('ignore')
 
 
 def main():
-    data_dir = 'data'
+    data_dir = r"C:\Users\Gayatri\Documents\Modular Image Classification\task_try\data"
     batch_size = 32
     epochs = 50
     ask_epoch = 10
@@ -41,7 +41,7 @@ def main():
     train_gen, val_gen = load_data(data_dir, batch_size=batch_size)
     model = build_model(num_classes=train_gen.num_classes, freeze=freeze)
 
-    lra_callback = LRA(patience=2, stop_patience=3, threshold=0.85, factor=0.5, dwell=True, ask_epoch=ask_epoch, model_name='MobileNetV2')
+    lra_callback = LRA(patience=2, stop_patience=3, threshold=0.8, factor=0.5, dwell=True, ask_epoch=ask_epoch, model_name='MobileNetV2')
     history = train_model(model, train_gen, val_gen, callbacks=[lra_callback], epochs=epochs)
     print("history---->", history)
 
